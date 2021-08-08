f7e122cd95511bab57656b7ca6619465166f8d37
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-08-07 21:20:48 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+from backend.controllers.client_controller import clients_model
+from backend.controllers.routine_controller import routine_model
+from backend.controllers.clients_routine_controller import routine_client_model
+
+app = Flask(__name__)
+
+app.register_blueprint(clients_model)
+app.register_blueprint(routine_model)
+app.register_blueprint(routine_client_model)
+
+cors = CORS(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ backend/controllers/client_controller.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+
+from flask_cors import CORS, cross_origin
+
+from backend.models.clients_model import ClientsModel
+
+clients_model = Blueprint('clients_model', __name__)
+
+model = ClientsModel()
+
+@clients_model.route('/client/create_client', methods=['POST'])
+@cross_origin()
+def create_client():
+    content = model.create_client(request.json['name'], request.json['email'], int(request.json['routines']))
+    return jsonify(content)
+
+@clients_model.route('/client/get_clients', methods=['GET'])
+@cross_origin()
+def get_categories():
+    return jsonify(model.get_clients())
+
+@clients_model.route('/client/delete_client', methods=['DELETE'])
+@cross_origin()
+def delete_category():
+    return jsonify(model.delete_client(int(request.json['id'])))
