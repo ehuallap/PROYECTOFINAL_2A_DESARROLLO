18a8a26f50d6b4473601409d5237ea923fc6e792
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-08-10 09:29:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,12 @@ from backend.controllers.routine_controller import routine_model
 from backend.controllers.clients_routine_controller import routine_client_model
 
 app = Flask(__name__)
-            # static_folder="./dist",
-            # template_folder="./frontend_2.0/dist"
 
 app.register_blueprint(clients_model)
 app.register_blueprint(routine_model)
 app.register_blueprint(routine_client_model)
 
-# cors = CORS(app)
 cors = CORS(app, resources={r"/client/*": {"origins": "*"}})
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def dender_vue(path):
-#     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
