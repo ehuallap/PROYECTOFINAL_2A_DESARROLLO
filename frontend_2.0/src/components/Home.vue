83a7f46e3d409246fc209b9d810db407e7229702
ehuallap <ehuallap@unsa.edu.pe>
2021-08-09 20:55:23 -0500

    Last

--- frontend_2.0/src/components/Home.vue
+++ frontend_2.0/src/components/Home.vue
@@ -2,7 +2,7 @@
   <h2> Bienvenido estimado {{Client}}</h2>
     <div class="text-center">
         <h1 class="text-warning mt-3 mb-3"> Ten el acceso a la información de seminarios y eventos nacionales e internacionales, esencial para estudiantes universitarios. </h1>
-        <img src="../assets/alumnos.jpg" class="mb-3" width="500" alt="alumnos">
+
     </div>
     <div class="text-center">
             <div>
